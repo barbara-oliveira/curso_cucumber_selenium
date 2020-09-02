602acf56b56c2950d5b311841fd42b6428055a91
Bárbara Oliveira da Silva <barbara@ciandt.com>
2020-09-02 17:37:00 -0300

    add screenshots

--- src/test/java/com/study/bdd/cucumber/steps/InserirContaSteps.java
+++ src/test/java/com/study/bdd/cucumber/steps/InserirContaSteps.java
@@ -1,13 +1,20 @@
 package com.study.bdd.cucumber.steps;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class InserirContaSteps {
 
@@ -67,7 +74,13 @@ public class InserirContaSteps {
         Assert.assertEquals(string, mensagem);
     }
 
-    @After
+    @After(order = 1)
+    public void screenshot(Scenario cenario) throws IOException {
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("target/screenshot/"+cenario.getId()+".jpg"));
+    }
+
+    @After(order = 0)
     public void fecharBrowser() {
         driver.quit();
     }
